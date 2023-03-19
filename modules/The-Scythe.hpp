bbd0e2de07d74afa5a0ac7efd54e3581aff2b333
The_Reaper <83348119+The-Reap-er@users.noreply.github.com>
2023-03-19 10:09:06 +0330

    Create The-Scythe.hpp

---
+++ modules/The-Scythe.hpp
@@ -0,0 +1,20 @@
+//
+// Created by grimreaper on 3/18/23.
+//
+#ifndef THE_SCYTHE_HPP
+#define THE_SCYTHE_HPP
+
+#include "core/aocinfo.hpp"
+#include "config.hpp"
+
+
+
+// mother of all namespaces
+namespace thescythe
+{
+
+}
+
+
+
+#endif //THE_SCYTHE_HPP
